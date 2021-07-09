d794d4711a0b5327662e025a772ee041f1798918
ju <1549465959@qq.com>
2021-07-09 11:01:33 +0800

    fix：文章上一篇下一篇位置相反问题

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -14,14 +14,14 @@
         </div>
         
         <div class="post-nav cf">
-          <div class="post-nav-prev post-nav-item">
-            <router-link :to="getRouteUrl(prev.id)" class="status-btn" v-if="prev.id">
-              <i class="iconfont">&#xe6a0;</i>{{prev.title}}
-            </router-link>
-          </div>
           <div class="post-nav-next post-nav-item">
             <router-link :to="getRouteUrl(next.id)" class="status-btn" v-if="next.id">
-              {{next.title}}<i class="iconfont">&#xe69f;</i>
+              <i class="iconfont">&#xe6a0;</i>{{next.title}}
+            </router-link>
+          </div>
+          <div class="post-nav-prev post-nav-item">
+            <router-link :to="getRouteUrl(prev.id)" class="status-btn" v-if="prev.id">
+              {{prev.title}}<i class="iconfont">&#xe69f;</i>
             </router-link>
           </div>
         </div>
